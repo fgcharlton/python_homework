c159af6e33069f83cda6f2e7aa2706b68b37c9c7
fgcharlton <fgcharlton1@gmail.com>
2026-07-13 19:25:14 -0400

    Updated based on AirHub feedback both diary.py and assignment2.py

--- assignment2/assignment2.py
+++ assignment2/assignment2.py
@@ -16,15 +16,7 @@ def read_employees():
         employee_dict["rows"] = rows
         return employee_dict
     except Exception as e:
-        trace_back = traceback.extract_tb(e.__traceback__)
-        stack_trace = list()
-        for trace in trace_back:
-            stack_trace.append(f'File : {trace[0]} , Line : {trace[1]}, Func.Name : {trace[2]}, Message : {trace[3]}')
-            print(f"Exception type: {type(e).__name__}")
-            message = str(e)
-            if message:
-                print(f"Exception message: {message}")
-                print(f"Stack trace: {stack_trace}")
+        print(f"An exception occurred: {e}")
    
 employees = read_employees()
 print(employees)
